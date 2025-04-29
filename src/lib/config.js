9de62334d3343fab0150fc98ac4f8f57436f33bc
Nguyễn Thị Ngọc Anh <22010040@st.phenikaa-uni.edu.vn>
2025-04-29 17:49:02 +0700

    Upload full project

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,15 +3,15 @@
  * in the <meta> tags, in the footer, and in the RSS feed.
  * 
  * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
- **/ 
+ **/
 
-export const siteTitle = 'My Awesome Blog'
-export const siteDescription = 'Built with the SvelteKit Static Blog Starter'
+export const siteTitle = ' Blog cua Nguyen Thi Ngoc Anh '
+export const siteDescription = 'Mon Ung dung phan tan '
 export const siteURL = 'example.com'
 export const siteLink = 'https://github.com/josh-collinsworth/sveltekit-blog-starter'
 export const siteAuthor = '- find and change this text in src/lib/config.js'
 
-// Controls how many posts are shown per page on the main blog index pages
+// Controls how many posts are shown per page on the main blog index pages	
 export const postsPerPage = 10
 
 // Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
@@ -24,6 +24,6 @@ export const navItems = [
 		route: '/about'
 	}, {
 		title: 'Contact',
-		route: '/contact' 
+		route: '/contact'
 	},
 ]
